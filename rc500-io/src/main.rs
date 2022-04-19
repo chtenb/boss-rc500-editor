b7ad888b3b767ca43e1fbd44a4b3a76ad58dc0c0
chtenb <ctenbrinke@gmail.com>
2022-04-19 15:27:46 +0200

    list storage devices

--- rc500-io/src/main.rs
+++ rc500-io/src/main.rs
@@ -1,4 +1,12 @@
 use clap::Parser;
+// use core::result;
+// use core::result::Result;
+use std::fs::copy;
+use std::path::Path;
+use std::path::PathBuf;
+use windows::core::Result;
+use windows::Devices::Enumeration::{DeviceClass, DeviceInformation, DeviceInformationCollection};
+use windows::Devices::Portable::StorageDevice;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -13,10 +21,41 @@ struct Args {
     count: u8,
 }
 
-fn main() {
-    let args = Args::parse();
+fn main() -> Result<()> {
+    // let foo = StorageFile::GetFileFromPathAsync("test");
+    // println!("{:?}", foo);
+    // let bar = StorageDevice::GetDeviceSelector();
+    // println!("{:?}", bar)
+    // StorageDevice::GetDeviceSelector()
+    //     .map
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    let fetch_infos =
+        DeviceInformation::FindAllAsyncDeviceClass(DeviceClass::PortableStorageDevice)?;
+    let infos = fetch_infos.get()?;
+    let nr_devs = infos.Size()?;
+    println!("Found {:?} devices", nr_devs);
+    for info in infos {
+        print_info(info)
+            .map_err(|e| {
+                println!("Error occurred while retrieving device info: {:?}", e)
+            });
     }
+    Ok(())
+}
+
+fn copy_files(device_root: &Path, to: &Path) -> Result<()> {
+    let from = PathBuf::new().join(device_root).join(Path::new(r"ROLAND\DATA\MEMORY1.RC0"));
+    println!("Copying {:?} to {:?}", from, to);
+    let result = copy(from, to);
+    println!("{:?}", result)
+}
+
+fn print_info(info: DeviceInformation) -> Result<()> {
+    let name = info.Name()?;
+    let id = info.Id()?;
+    println!("{:?} {:?}", id, name);
+    let storage = StorageDevice::FromId(id)?;
+    let path = storage.Path()?;
+    println!("Path: {}", path);
+    Ok(())
 }
